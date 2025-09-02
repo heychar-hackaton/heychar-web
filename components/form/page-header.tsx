2e5b315b536b5e44e905048de3a364c91f59bc2e
MAVRIN <soman@inbox.ru>
2025-09-02 12:01:37 +0300

    fix: уточнено описание поля recommendation и улучшен стиль компонента PageHeader

--- components/form/page-header.tsx
+++ components/form/page-header.tsx
@@ -1,25 +1,27 @@
 import React, { type ReactNode } from "react"
 
 const PageHeader = ({
-  title,
-  description,
-  actions,
+    title,
+    description,
+    actions,
 }: {
-  title: ReactNode
-  description?: ReactNode
-  actions?: ReactNode
+    title: ReactNode
+    description?: ReactNode
+    actions?: ReactNode
 }) => {
-  return (
-    <div className="mb-2">
-      <div className="flex items-center justify-between">
-        <h1 className="font-semibold text-lg">{title}</h1>
-        {actions}
-      </div>
-      {description && (
-        <span className="text-muted-foreground text-sm">{description}</span>
-      )}
-    </div>
-  )
+    return (
+        <div className="">
+            <div className="flex items-center justify-between">
+                <h1 className="font-semibold text-lg">{title}</h1>
+                {actions}
+            </div>
+            {description && (
+                <span className="text-muted-foreground text-sm">
+                    {description}
+                </span>
+            )}
+        </div>
+    )
 }
 
 export default PageHeader
